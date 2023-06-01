2a5296f4fb45e1f6e206cc7b12f57b70723e7f0f
AryaX2022 <6983299@gmail.com>
2023-06-01 18:04:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from interact2 import execute
 
 def greet(name):
     ret = execute(name)
-    return "Hello " + name + "!"
+    return ret
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
